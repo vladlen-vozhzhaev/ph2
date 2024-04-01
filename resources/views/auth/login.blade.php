00016398046b1c505dda7fe832ca03d45d1746b2
Vlalden <vladlen@vozhzhaev.ru>
2024-04-01 16:16:07 +0300

    first

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div class="container my-5">
+    <h2 class="text-center my-3">Авторизация на сайте</h2>
+    <div class="col-sm-6 mx-auto">
+        <form action="/login" method="POST">
+            @csrf
+            <div class="mb-3">
+                <input name="email" type="email" class="form-control" placeholder="Email">
+            </div>
+            <div class="mb-3">
+                <input name="password" type="password" class="form-control" placeholder="Пароль">
+            </div>
+            <div class="mb-3">
+                <input type="submit" class="form-control btn btn-primary" value="Зарегистрироваться">
+            </div>
+        </form>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+{{--<x-guest-layout>--}}
+{{--    <!-- Session Status -->--}}
+{{--    <x-auth-session-status class="mb-4" :status="session('status')" />--}}
+
+{{--    <form method="POST" action="{{ route('login') }}">--}}
+{{--        @csrf--}}
+
+{{--        <!-- Email Address -->--}}
+{{--        <div>--}}
+{{--            <x-input-label for="email" :value="__('Email')" />--}}
+{{--            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />--}}
+{{--            <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Password -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="password" :value="__('Password')" />--}}
+
+{{--            <x-text-input id="password" class="block mt-1 w-full"--}}
+{{--                            type="password"--}}
+{{--                            name="password"--}}
+{{--                            required autocomplete="current-password" />--}}
+
+{{--            <x-input-error :messages="$errors->get('password')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Remember Me -->--}}
+{{--        <div class="block mt-4">--}}
+{{--            <label for="remember_me" class="inline-flex items-center">--}}
+{{--                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">--}}
+{{--                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>--}}
+{{--            </label>--}}
+{{--        </div>--}}
+
+{{--        <div class="flex items-center justify-end mt-4">--}}
+{{--            @if (Route::has('password.request'))--}}
+{{--                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">--}}
+{{--                    {{ __('Forgot your password?') }}--}}
+{{--                </a>--}}
+{{--            @endif--}}
+
+{{--            <x-primary-button class="ms-3">--}}
+{{--                {{ __('Log in') }}--}}
+{{--            </x-primary-button>--}}
+{{--        </div>--}}
+{{--    </form>--}}
+{{--</x-guest-layout>--}}
---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/addItem', function (){
+    return view('pages.addItem');
+});
+Route::post('/addItem', function (\Illuminate\Http\Request $request){
+    $product = new Product();
+    $product->name = $request->name;
+    $product->description = $request->description;
+    $product->cost = $request->cost;
+    $product->save();
+    return "Товар успешно добавлен!";
+});
+Route::get('/shop', function (){
+    $products = Product::all(); // SELECT * FROM products
+    return view('pages.shop', ['products'=>$products]);
+});
+Route::get('/shop/{id}', function (\Illuminate\Http\Request $request){
+   $product = Product::where('id', $request->id)->first();
+   return view('pages.singleProduct', ['product'=>$product]);
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
